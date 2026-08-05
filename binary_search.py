c114ae8335616ea7eb04bf1813bb229d21863d75
Kaleb-TAFE <kaleb.m2005@outlook.com>
2026-08-05 10:34:50 +0800

    Changed the way the middle value was being calculated.
    Changed elif statement executions to save the actual position the number being looked at is in the list.

--- binary_search.py
+++ binary_search.py
@@ -2,12 +2,12 @@ def find(search_list, value):
     lowest = 0
     highest = len(search_list) - 1
     while lowest <= highest:
-        middle = highest - lowest
+        middle = (highest - lowest) // 2
         if search_list[middle] == value:
             return search_list.index(search_list[middle])
         elif search_list[middle] > value:
-            highest -= 1
+            highest -= search_list.index(search_list[middle])
         elif search_list[lowest] < value:
-            lowset += 1
+            lowset += search_list.index(search_list[middle])
         
     raise ValueError("value not in array")

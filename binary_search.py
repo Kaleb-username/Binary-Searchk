29d07d170b691d204ad5859461a5aec758cd172d
Kaleb-TAFE <kaleb.m2005@outlook.com>
2026-08-05 10:36:45 +0800

    Fixed the way the middle value was being calculated. (Cannot believe I forgot simple median maths)
    Fixed the way values in the search list being scanned would iterate the "highest" or "lowest" variables.

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,14 @@
 def find(search_list, value):
     lowest = 0
     highest = len(search_list) - 1
+
     while lowest <= highest:
-        middle = (highest - lowest) // 2
+        middle = (highest + lowest) // 2
         if search_list[middle] == value:
             return search_list.index(search_list[middle])
         elif search_list[middle] > value:
-            highest -= search_list.index(search_list[middle])
+            highest = middle - 1
         elif search_list[lowest] < value:
-            lowset += search_list.index(search_list[middle])
+            lowest = middle + 1
         
     raise ValueError("value not in array")

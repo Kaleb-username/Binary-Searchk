f4bbcb4fed3e2f7b4c74e5e5169740a86343ab31
Kaleb-TAFE <kaleb.m2005@outlook.com>
2026-08-05 10:31:20 +0800

    Properly fixed highest value so it wouldn't go out of range.
    Removed previous "scouting method".
    
    Changed if statement to check if the value being looked at in the list is correct along with what is being returned when number has been found.

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,10 @@
 def find(search_list, value):
     lowest = 0
-    highest = len(search_list - 1)
+    highest = len(search_list) - 1
     while lowest <= highest:
-        middle = search_list[highest - lowest]
-        if middle == value:
-            return middle
-        elif middle > value:
-            search_list[middle:].pop
-        elif search_list < value:
-            search_list[:middle].pop
+        middle = highest - lowest
+        if search_list[middle] == value:
+            return search_list.index(search_list[middle])
+        
+        
     raise ValueError("value not in array")
